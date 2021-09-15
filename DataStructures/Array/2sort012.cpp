a37eda9db69a722a1f7042c300289ffb23b76a01
vinay030901 <champ.vk91@gmail.com>
2021-09-15 10:30:41 +0530

    enter

--- DataStructures/Array/2sort012.cpp
+++ DataStructures/Array/2sort012.cpp
@@ -3,5 +3,28 @@
 using namespace std;
 int main()
 {
-    //we will follow the dutch flag algorithm for
+    //we will follow the dutch flag algorithm
+    int arr[]={2,0,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int low=0,mid=0,high=n-1;
+    while(mid<=high) {
+        if(arr[mid]==0)
+            {
+                swap(arr[mid],arr[low]);
+                low++;
+                mid++;
+            }
+        else if(arr[mid]==1)
+        {
+            mid++;
+        }
+        else if(arr[mid]==2)
+        {
+            swap(arr[mid],arr[high]);
+            
+            high--;
+        }
+    }
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
 }
