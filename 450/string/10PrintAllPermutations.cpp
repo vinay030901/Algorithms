3414482b1aca0b8ef01feeb8a3354d6e711a4329
vinay030901 <champ.vk91@gmail.com>
2022-02-01 14:08:14 +0530

    enter

---
+++ 450/string/10PrintAllPermutations.cpp
@@ -0,0 +1,16 @@
+/*
+A permutation also called an “arrangement number” or “order,” is a rearrangement of the elements of an 
+ordered list S into a one-to-one correspondence with S itself. A string of length n has n! permutation. 
+
+
+Below are the permutations of string ABC. 
+ABC ACB BAC BCA CBA CAB*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    string str="abc";
+    int n=str.length();
+    
+}
